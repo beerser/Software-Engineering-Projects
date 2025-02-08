6b2043735b774de72df69485c83d96cb2931299b
Phubet66040 <s6604062636569@email.kmutnb.ac.th>
2025-02-08 12:48:47 +0700

    check css

--- Srisuwan/src/main.jsx
+++ Srisuwan/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-im
+import 'bootstrap/dist/css/bootstrap.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
