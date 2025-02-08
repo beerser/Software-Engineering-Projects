7851c42fa73815afd1ac070366707f2a1b0e1b7e
Phubet66040 <s6604062636569@email.kmutnb.ac.th>
2025-02-08 12:55:30 +0700

    NavBar set
    setting navbartab

--- Srisuwan/src/components/Navbar.jsx
+++ Srisuwan/src/components/Navbar.jsx
@@ -3,40 +3,31 @@ import React, { useState } from "react";
 const Navbar = ({ setdata }) => {
     const [show, setshow] = useState(false);
     const [n, setN] = useState([
-        {name:"Ai",detail:"ASUS"},
-        {name:"Au",detail:"BKO"},
+        { name: "Ai", detail: "ASUS" },
+        { name: "Au", detail: "BKO" },
     ]);
- 
- 
- 
- 
+
+
+
+
     const handdleClick = () => {
-    setN(n + 1);
-    setshow(true)
-    if(show){
-        alert("28259")
-    }
-  };
+        setN(n + 1);
+        setshow(true)
+        if (show) {
+            alert("28259")
+        }
+    };
 
-  return (
-    <div>
-      {n ? <>True</> : <>False</>}
-      Navbar
-    <ul className="title-text">
-    {n.map((item,index)=><li key={index}>{item.name}:{item.detail}</li>)}
-
-    </ul>
-
-
-      <button
-        onClick={() => {
-          handdleClick();
-        }}
-      >
-        Click
-      </button>
-    </div>
-  );
+    return (
+        <><nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    
+                    Srisuwan
+                </a>
+            </div>
+        </nav></>
+    );
 };
 
 export default Navbar;
