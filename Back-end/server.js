e258af502c4c1ed68f18e9dd31c9a6f86b63771e
Phubet66040 <s6604062636569@email.kmutnb.ac.th>
2025-02-11 12:48:58 +0700

    Add code by Unknowkubbrother
    He help fix API code thx and we will praise him.

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,9 +1,54 @@
 const express = require('express');
-const Quote = require('inspirational-quotes')
+// const Quote = require('inspirational-quotes')
+const cors = require('cors');
+const generatePayload = require('promptpay-qr');
+const qrcode = require('qrcode');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const compression = require('compression');
 
 const app = express();
+
+app.use(cors({
+    origin: true,
+    credentials: true,
+}));
+
+
+// app.use(compression());
+app.use(bodyParser.json());
+
 app.get('/',(req,res)=>{
-    res.setDefaultEncoding()
+    return res.send("hello");
+})
+
+app.post('/generateQR',(req,res)=>{
+    try {
+        let mobileNumber = req.body.phone || '000-000-0000';
+        let amount = req.body.amount || 0;
+
+
+        const generQRCode = (payload) => {
+            const options = { type: 'svg', color: { dark: '#000', light: '#fff' } };
+            qrcode.toString(payload, options, (err, svg) => {
+                if (err) return console.log(err)
+                fs.writeFileSync(`QRCODE/QRCODE-${Date.now()}.svg`, svg)
+            })
+        }
+
+        if (mobileNumber == "000-000-0000"){
+            return res.statusCode(404);
+        }
+
+        const payload = generatePayload(mobileNumber, {amount})
+        generQRCode(payload);
+        return res.send(payload)
+
+    }catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+
 })
 
 app.listen(5000,()=>{
