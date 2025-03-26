671f6a71327ad471abf6fc91d45231c877c1c2b7
Phubet66040 <s6604062636569@email.kmutnb.ac.th>
2025-03-27 02:24:17 +0700

    fix

--- Srisuwan/src/components/Neary.jsx
+++ Srisuwan/src/components/Neary.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../css/Neary.css";
 import TU from "../assets/TU.jpg";
 import RU from "../assets/RU.jpg";
@@ -11,21 +11,29 @@ const Neary = () => {
     { name: 'Bangkok University', image: BU }
   ];
 
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
   return (
     <div className="nearby-institutions-container">
-     
+      {/* รูปหลักที่เปลี่ยนได้ */}
       <div className="main-image">
-        <img src={TU} alt="Thammasat University" />
+        <img src={institutions[selectedIndex].image} alt={institutions[selectedIndex].name} />
       </div>
 
-    
       <div className="right-container">
         <div className="institutions-title">
           <h2>Which educational institutions are nearby?</h2>
         </div>
+
+        <div className="thumbnail-indicator" style={{ left: `${selectedIndex * 120}px` }}></div>
+
         <div className="thumbnail-grid">
           {institutions.map((institution, index) => (
-            <div key={index} className="thumbnail-card">
+            <div 
+              key={index} 
+              className={`thumbnail-card ${selectedIndex === index ? 'active' : ''}`}
+              onClick={() => setSelectedIndex(index)}
+            >
               <img src={institution.image} alt={institution.name} />
               <div className="institution-name">{institution.name.split(' ')[0]}</div>
             </div>
