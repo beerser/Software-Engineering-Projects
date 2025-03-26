d837ebdfbf6b45a81726648ccf63625a9638e07a
Phubet66040 <s6604062636569@email.kmutnb.ac.th>
2025-03-26 13:33:05 +0700

    make docker

--- Srisuwan/src/main.jsx
+++ Srisuwan/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./css/index.css";
 import Home  from "./page/Home.jsx";
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { AuthProvider } from "./components/AuthContext"; 
 
 createRoot(document.getElementById("root")).render(
