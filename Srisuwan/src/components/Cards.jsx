516b7d6a2d0a8101f7744bf6f608b483a0d5839d
Phubet66040 <s6604062636569@email.kmutnb.ac.th>
2025-03-30 12:50:44 +0700

    fix for au

--- Srisuwan/src/components/Cards.jsx
+++ Srisuwan/src/components/Cards.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import "../css/Cards.css";
 const Cards = ({ obj, onPaymentClick }) => {
   const navigate = useNavigate();
 
