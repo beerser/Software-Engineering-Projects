fad2b9a8f8092eb8df8241aba923cee2f4646536
Thanakorn Jaithon <bearjaiton@gmail.com>
2026-01-29 02:37:19 +0700

    Update vite.config.js

--- Srisuwan/vite.config.js
+++ Srisuwan/vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Software-Engineering-Projects/',
+  base: '/Software-Engineering-Projects/', 
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',   
-    port: 5173       
-  }
 })
